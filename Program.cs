93f549c6e731c9d005a319f6a63bb2c9d283ce06
User <User@DESKTOP-7DEACCV>
2024-07-05 09:42:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_19_21_22
+{
+    class Program
+    {
+        public static List<Book> books = new List<Book>();
+
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void SaveBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void LoadBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void AddBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void BorrowBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ReturnBook()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ListAvailableBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ListBorrowedBooks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
